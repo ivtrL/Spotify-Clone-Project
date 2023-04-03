0f6be7fa6c13d7e2d0da680ea59aee9ce55e8781
ivtrL <isaacvitorinola@outlook.com>
2023-04-03 11:24:04 -0300

    PlayerBar config

--- src/components/ActuallyListeningBar.jsx
+++ src/components/ActuallyListeningBar.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../css/ActuallyListeningBar.css"
+import { Slider, Stack } from '@mui/material';
+import { VolumeDown, VolumeUp } from '@mui/icons-material';
 
 const ActuallyListeningBar = () => {
+
+  const [volumeValue, setVolumeValue] = useState(30)
+
   return (
     <div className='listening-bar'>
       <div className="listening-bar-container">
         <p>Actual music playing:</p>
         <p>Controller Bar</p>
-        <p>Volume and Misc</p>
+        <div className='volume-bar'>
+          <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+            {volumeValue == 50 ? <VolumeUp />: <VolumeDown/>}
+            <Slider sx={{color: 'white'/*,'& .MuiSlider-thumb': {borderRadius: '10px', transform: 'scale(0.1rem)'}*/}} value={volumeValue} aria-label="Default" valueLabelDisplay='auto'/>
+          </Stack>
+        </div>
       </div>
     </div>
   )
