a4c4dc2a2a6a7698474aedb63337f56e677e15f3
ivtrL <isaacvitorinola@outlook.com>
2023-04-03 21:09:40 -0300

    VolumeBar and Dynamic Icon added

--- src/components/ActuallyListeningBar.jsx
+++ src/components/ActuallyListeningBar.jsx
@@ -1,21 +1,33 @@
 import React, { useState } from 'react'
 import "../css/ActuallyListeningBar.css"
-import { Slider, Stack } from '@mui/material';
-import { VolumeDown, VolumeUp } from '@mui/icons-material';
+import Slider from '@mui/material/Slider'
+import Stack from '@mui/material/Stack'
+import VolumeDownRoundedIcon from '@mui/icons-material/VolumeDownRounded' 
+import VolumeUpRoundedIcon from '@mui/icons-material/VolumeUpRounded'
+import VolumeMuteRoundedIcon from '@mui/icons-material/VolumeMuteRounded'
+import VolumeOffRoundedIcon from '@mui/icons-material/VolumeOffRounded'
 
 const ActuallyListeningBar = () => {
 
-  const [volumeValue, setVolumeValue] = useState(30)
+  const [iconMute , setIconMute] = useState(false)
+  const [value, setValue] = useState(30)
 
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    setIconMute(false)
+  };
+  
   return (
     <div className='listening-bar'>
       <div className="listening-bar-container">
         <p>Actual music playing:</p>
         <p>Controller Bar</p>
         <div className='volume-bar'>
-          <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-            {volumeValue == 50 ? <VolumeUp />: <VolumeDown/>}
-            <Slider sx={{color: 'white'/*,'& .MuiSlider-thumb': {borderRadius: '10px', transform: 'scale(0.1rem)'}*/}} value={volumeValue} aria-label="Default" valueLabelDisplay='auto'/>
+          <Stack direction="row" spacing={2}>
+            <a href="#" style={{color: 'white'}} onClick={() => {iconMute ? setIconMute(false): setIconMute(true)}}>
+              {!iconMute && (value == 0 ? <VolumeMuteRoundedIcon/>: value >= 50 ? <VolumeUpRoundedIcon/>: <VolumeDownRoundedIcon/>)}{iconMute && <VolumeOffRoundedIcon/>}
+            </a>
+            <Slider size='small' sx={{color: 'white'}} value={value} onChange={handleChange} aria-label="Default" valueLabelDisplay='auto'/>
           </Stack>
         </div>
       </div>
