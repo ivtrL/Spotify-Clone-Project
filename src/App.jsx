e84c445502d5c6c63e14f900369de4b068544942
ivtrL <isaacvitorinola@outlook.com>
2023-04-05 00:12:24 -0300

    prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import ActuallyListeningBar from "./components/ActuallyListeningBar"
-import Home from "./components/Home"
-import Navbar from "./components/Navbar"
-import Playlists from "./components/Playlists"
-import Profile from "./components/Profile"
-import Login from "./components/Login"
+import ActuallyListeningBar from "./components/ActuallyListeningBar";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Playlists from "./components/Playlists";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
 
 function App() {
-
   return (
     <div>
-      <Login/>
-      <Navbar/>
-      <Profile/>
-      <Home/>
-      <Playlists/>
-      <ActuallyListeningBar/>
+      <Login />
+      <Navbar />
+      <Profile />
+      <Home />
+      <Playlists />
+      <ActuallyListeningBar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
